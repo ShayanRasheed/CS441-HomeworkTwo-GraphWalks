ae58a99df1f9b87f1be31323912856d3e751e6f2
Shayan Rasheed <shayanrasheed001@gmail.com>
2023-10-28 16:18:28 -0500

    Initial Commit

---
+++ src/main/scala/GraphLoader.scala
@@ -0,0 +1,18 @@
+package com.lsc
+
+import org.apache.spark.graphx.Graph
+import org.slf4j.LoggerFactory
+import NetGraphAlgebraDefs.NetGraphComponent
+
+import java.io.{FileNotFoundException, IOException}
+
+object GraphLoader {
+  private val logger = LoggerFactory.getLogger(this.getClass)
+  private val temp = "Done"
+
+  def loadGraph(filePath: String): String = {
+
+
+    temp
+  }
+}
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,34 @@
+package com.lsc
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.graphx._
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("GraphWalk").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    // Define vertices and edges
+    val vertices = sc.makeRDD(Array((1L, "Alice"), (2L, "Bob"), (3L, "Charlie"), (4L, "David")))
+    val edges = sc.makeRDD(Array(Edge(1L, 2L, "Friend"), Edge(2L, 3L, "Follows"), Edge(3L, 4L, "Friend")))
+
+    // Create a Graph
+    val defaultVertex = ("John Doe")
+    val graph = Graph(vertices, edges, defaultVertex)
+
+    // Basic GraphX operations
+    println("Vertices:")
+    graph.vertices.foreach { case (id, name) => println(s"$id: $name") }
+
+    println("Edges:")
+    graph.edges.foreach { edge => println(s"${edge.srcId} --${edge.attr}--> ${edge.dstId}") }
+
+    // Applying a graph algorithm: PageRank
+    val pageRank = graph.pageRank(0.1).vertices
+    println("PageRank results:")
+    pageRank.foreach { case (id, rank) => println(s"$id: $rank") }
+
+    sc.stop()
+  }
+}

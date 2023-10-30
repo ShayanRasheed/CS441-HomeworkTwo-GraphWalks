3b70077a7e640aad3d8dc6d6034113648ecaa436
Shayan Rasheed <shayanrasheed001@gmail.com>
2023-10-30 13:55:06 -0500

    Added example graph to load

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,39 +2,34 @@ package com.lsc
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx._
-import GraphLoader.loadGraph
+import GraphLoader.{getClass, loadGraph}
+import NetGraphAlgebraDefs.{Action, NodeObject}
+import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 
 object Main {
+  val config = ConfigFactory.load()
+  private val logger = LoggerFactory.getLogger(getClass)
 
   def main(args: Array[String]): Unit = {
-    //val conf = new SparkConf().setAppName("GraphWalk").setMaster("local")
-    //val sc = new SparkContext(conf)
+    val conf = new SparkConf().setAppName("GraphWalk").setMaster("local[4]")
+    val sc = new SparkContext(conf)
 
-//    // Define vertices and edges
-//    val vertices = sc.makeRDD(Array((1L, "Alice"), (2L, "Bob"), (3L, "Charlie"), (4L, "David")))
-//    val edges = sc.makeRDD(Array(Edge(1L, 2L, "Friend"), Edge(2L, 3L, "Follows"), Edge(3L, 4L, "Friend")))
-//
-//    // Create a Graph
-//    val defaultVertex = ("John Doe")
-//    val graph = Graph(vertices, edges, defaultVertex)
-//
-//    // Basic GraphX operations
-//    println("Vertices:")
-//    graph.vertices.foreach { case (id, name) => println(s"$id: $name") }
-//
-//    println("Edges:")
-//    graph.edges.foreach { edge => println(s"${edge.srcId} --${edge.attr}--> ${edge.dstId}") }
-//
-//    // Applying a graph algorithm: PageRank
-//    val pageRank = graph.pageRank(0.1).vertices
-//    println("PageRank results:")
-//    pageRank.foreach { case (id, rank) => println(s"$id: $rank") }
+    val graph = loadGraph(config.getString("App.originalFilePath"), sc)
 
-    val list = loadGraph("/Users/shayanrasheed/Documents/Cloud Computing/NetGameSimNetGraph_21-10-23-12-20-35.ngs")
+    graph match {
+      case Some(graph) =>
+        logger.info("Beginning random walks on graph:")
 
-    println("Got here")
-    list.foreach(x => println(x))
+        val startNode = graph.vertices.takeSample(withReplacement = false, 1)(0)._1
 
-    //sc.stop()
+        logger.info(s"Beginning with Vertex: $startNode")
+
+        
+      case None =>
+        logger.warn("MAIN: Graph Failed to load")
+    }
+
+    sc.stop()
   }
 }

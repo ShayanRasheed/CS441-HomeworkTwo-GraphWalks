5e55d2069b30aa9bb7716e0e7754b12a417c661d
Shayan Rasheed <shayanrasheed001@gmail.com>
2023-10-31 12:30:24 -0500

    Finished SimRank

--- src/main/scala/SimRank.scala
+++ src/main/scala/SimRank.scala
@@ -12,28 +12,39 @@ import scala.language.postfixOps
 class SimRank {
   private val logger = LoggerFactory.getLogger(getClass)
   def calculateSimRank(graph1: Graph[NodeObject, Action], graph2: Graph[NodeObject, Action], node1: VertexId, node2: VertexId, depth: Int): Double = {
-    logger.info("In Sim Rank:")
+    logger.trace("In Sim Rank:")
 
-    val neighbors1 = findNeighbors(graph1, node1, depth, ListBuffer.empty)
+    val neighbors1 = findNeighbors(graph1, node1, depth, List.empty)
     neighbors1.foreach(x => println(x))
 
+    val neighbors2 = findNeighbors(graph2, node2, depth, List.empty)
+
+    val commonNeighbors = neighbors1.intersect(neighbors2)
+    val totalUniqueNeighbors = (neighbors1 ++ neighbors2).distinct.size
+
     logger.trace(s"Sim Rank for Node $node1 and Node $node2 completed")
-    0.0
+    if(totalUniqueNeighbors > 0) {
+      commonNeighbors.size.toDouble / totalUniqueNeighbors
+    }
+    else {
+      0.0
+    }
   }
 
-  private def findNeighbors(graph: Graph[NodeObject, Action], node: VertexId, depth: Int, result: ListBuffer[VertexId]) : ListBuffer[VertexId] = {
-    val neighbors = graph.collectNeighbors(EdgeDirection.Either).lookup(node)
-    neighbors.foreach(x => println(x.mkString("Array(", ", ", ")")))
+  private def findNeighbors(graph: Graph[NodeObject, Action], node: VertexId, depth: Int, visitedNodes: List[VertexId]) : List[VertexId] = {
+    val updatedVisitedNodes = visitedNodes :+ node
 
-    if(depth <= 0 || result.contains(node)) {
-      return result
+    if (depth > 0) {
+      val neighbors = graph.collectNeighbors(EdgeDirection.Either).lookup(node)
+      val unvisitedNeighbors = neighbors.head.filter { case (neighborId, _) =>
+        !visitedNodes.contains(neighborId)
+      }
+
+      unvisitedNeighbors.foldLeft(updatedVisitedNodes) { (acc, neighbor) =>
+        findNeighbors(graph, neighbor._1, depth - 1, acc)
+      }
+    } else {
+      updatedVisitedNodes
     }
-
-    val list = neighbors.head
-    for ((neighborId, _) <- list) {
-      findNeighbors(graph, neighborId, depth - 1, result)
-    }
-
-    result
   }
 }
